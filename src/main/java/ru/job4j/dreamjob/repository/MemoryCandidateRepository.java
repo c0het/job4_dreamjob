0d0ceb2b7b8d4116b3679ee54083c9a30d47d929
c0het <c0het401@gmail.com>
2023-12-11 20:41:51 +0300

    5. Список кандидатов [#504842]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -47,7 +47,8 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public boolean update(Candidate candidate) {
-        return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) -> new Candidate(oldCandidate.getId(), candidate.getName())) != null;
+        return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) ->
+                new Candidate(oldCandidate.getId(), candidate.getName(), candidate.getDescription(), candidate.getCreationDate())) != null;
     }
 
     @Override
