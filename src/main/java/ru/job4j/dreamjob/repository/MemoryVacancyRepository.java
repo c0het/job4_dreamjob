23e4e5b0cc2abae2ea79691aafcf44d8ad1cdc95
c0het <c0het401@gmail.com>
2023-12-18 07:48:30 +0300

    5. Просмотр, редактирование и удаление вакансии [#504850]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -38,8 +38,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
     }
 
     @Override
-    public void deleteById(int id) {
-        vacancies.remove(id);
+    public Vacancy deleteById(int id) {
+        return vacancies.remove(id);
     }
 
     @Override
