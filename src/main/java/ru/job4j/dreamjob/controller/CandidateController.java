47900837e1f8ce1759ae88fa394f8c4e367a6a39
c0het <c0het401@gmail.com>
2023-12-29 07:58:42 +0300

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -6,16 +6,18 @@ import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import ru.job4j.dreamjob.service.CandidateService;
+import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @Controller
 @RequestMapping("/candidates")
 public class CandidateController {
 
-    private final CandidateRepository candidateRepository = MemoryCandidateRepository.getInstance();
+    private final CandidateService candidateService = SimpleCandidateService.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("candidates", candidateRepository.findAll());
+        model.addAttribute("candidates", candidateService.findAll());
         return "candidates/list";
     }
 
@@ -26,7 +28,7 @@ public class CandidateController {
 
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
-        var candidateOptional = candidateRepository.findById(id);
+        var candidateOptional = candidateService.findById(id);
         if (candidateOptional.isEmpty()) {
             model.addAttribute("message", "Кандидат не найден");
             return "errors/404";
@@ -37,7 +39,7 @@ public class CandidateController {
 
     @PostMapping("/update")
     public String update(@ModelAttribute Candidate candidate, Model model) {
-        var isUpdate = candidateRepository.update(candidate);
+        var isUpdate = candidateService.update(candidate);
         if (!isUpdate) {
             model.addAttribute("message", "Кандидат не найден");
             return "errors/404";
@@ -47,7 +49,7 @@ public class CandidateController {
 
     @GetMapping("/delete/{id}")
     public String delete(Model model, @PathVariable int id) {
-        var idDeleted = candidateRepository.deleteById(id);
+        var idDeleted = candidateService.deleteById(id);
         if (idDeleted == null) {
             model.addAttribute("message", "Кандидат не найден");
             return "errors/404";
