bb57a4c3b4ec824b24c56038a32ad70a52260a1a
c0het <c0het401@gmail.com>
2024-01-17 07:18:27 +0300

    1. Формы. Поля ввода. [#504853 #473950]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -40,7 +40,11 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public boolean update(Vacancy vacancy) {
-        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate(), vacancy.getVisible())) != null;
+        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(),
+                vacancy.getTitle(),
+                vacancy.getDescription(),
+                vacancy.getCreationDate(),
+                vacancy.getVisible())) != null;
     }
 
     @Override
